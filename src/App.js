832d38a6ba3b56e3dd1d3e336d5a7a26037b8e89
Muhabat Khan Niazi <muhabatkhanx@gmail.com>
2022-09-07 08:14:09 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import {Home,Navigation} from "./constrains/index";
+import './style/style.css'
+
+function App() {
+  return (
+    <div className='app' >
+      <Navigation/>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
