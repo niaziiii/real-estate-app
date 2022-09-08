5d0feb3255d7addd1db148fbe397a2e47767ed23
Muhabat Khan Niazi <muhabatkhanx@gmail.com>
2022-09-08 11:16:22 -0700

    added updates to filter search product

---
+++ src/components/SearchProduct/searchFIelds.js
@@ -0,0 +1,52 @@
+const fieldsObj = {
+    location: [
+        {
+            value : '#',
+            text : 'All Locations'
+        },
+        {
+        value : 'multan',
+        text : 'multan'
+    },
+    {
+        value : 'Islamabad',
+        text : 'Islamabad'
+    },
+    {
+        value : 'lahore',
+        text : 'lahore'
+    },
+    {
+        value : 'peshawar',
+        text : 'peshawar'
+    },
+],
+type : [
+    {
+        value : 'villa',
+        text : 'villa'
+    },
+    {
+        value : 'apartment',
+        text : 'apartment'
+    },
+    {
+        value : 'commercial',
+        text : 'commercial Villa'
+    },
+    {
+        value : 'office',
+        text : 'office'
+    },
+],
+minBed : ['Min-Beds',1,2,3,4,5,6,7,8,9,10],
+maxBed : ['Max-Beds',1,2,3,4,5,6,7,8,9,10],
+minPrice : ['Minimum-Price',10000,20000,30000,40000,50000,60000,70000],
+maxPrice : ['Max-Price',10000,20000,30000,40000,50000,60000,70000]
+}
+
+export {
+    fieldsObj
+}
+
+
--- src/constrains/home/Home.jsx
+++ src/constrains/home/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-import {Carousel} from "../../components/index";
+import {Carousel, SearchProduct} from "../../components/index";
 
 function Home() {
   return (
     <div className='Carousel-Home'>
       <Carousel/>
+      <SearchProduct/>
     </div>
   )
 }
+
+
 
 export default Home
