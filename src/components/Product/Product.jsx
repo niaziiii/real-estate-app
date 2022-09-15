94db63a97cbd3d7deadc7261cf914f037e45937f
Muhabat Khan Niazi <muhabatkhanx@gmail.com>
2022-09-15 11:19:24 -0700

    updates to property card

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,22 +1,35 @@
 import React from 'react'
+import { FaBed, FaBath,FaLocationArrow } from "react-icons/fa";
+import { BsTextareaResize } from "react-icons/bs";
+import { useNavigate } from 'react-router-dom';
 
-import img1 from './../../assests/IMGS/vila1.jpg'
 
-function Product() {
+function Product(prop) {
+  const navigate = useNavigate();
+
+  const data = prop.data;
+  const handlerOverview = ()=>{
+    navigate(`/property/${data.id}`)
+  }
   return (
-    <div className='app__Product'>
+    <div className='app__Product' onClick={()=> handlerOverview()}>
       <div className="app__Product__container">
     <div className="app__Product__container__img">
-        <img src={img1} alt="Products" />
+        <img src={data.coverImages[0]} alt="Products" />
         <p className='slide-bottom'>View Details</p>
     </div>
     <div className="app__Product__container__content">
        <div className="app__Product__container__content__text">
-       <h2>spacious-4br-maid-study-shared-pool-gym-3</h2>
-        <span>➕ Umm Suqeim, Dubai</span>
-        <p><b>AED 300,000</b></p>
+       <h2>{data.nameProperty}</h2>
+        <span className='app__Product__container__content__location'> {<FaLocationArrow/>}{data.location}</span>
+        <p><b>AED {data.price}</b></p>
        </div>
-       <div className="app__Product__container__content__icons"></div>
+       <div className="app__Product__container__content__icons">
+        <div className="app__Product__container__content__icons__icon"> <FaBed/> <b>{data.beds}</b></div>
+        <div className="app__Product__container__content__icons__icon"> <FaBath/> <b>{data.bathrooms}</b></div>
+        <div className="app__Product__container__content__icons__icon"> <BsTextareaResize/> <b>{data.area}</b></div>
+
+       </div>
         
     </div>
         
