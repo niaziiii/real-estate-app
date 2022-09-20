e1008c56dd4a0f15ac54db923c8a0e20efbed496
Muhabat Khan Niazi <muhabatkhanx@gmail.com>
2022-09-20 11:11:41 -0700

    added contact router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import {Home,ProductOverviewPage,SearchPageProduct} from "./pages/index";
+import {Home,ProductOverviewPage,SearchPageProduct,About} from "./pages/index";
 import './style/style.css'
 import {
   Routes,
   Route,
 } from "react-router-dom";
+import Navigation from "./pages/Navigation/Navigation";
 
 
 function App() {
   return (
     <div className='app' >
+      <Navigation/>
       <Routes>
       <Route path="/" element={<Home/>}> </Route>
       <Route path="/property/:productId" element={<ProductOverviewPage/>}> </Route>
       <Route path="/properties" element={<SearchPageProduct/>}> </Route>
+      <Route path="/contact" element={<About/>}> </Route>
+
     </Routes>
       
     </div>
