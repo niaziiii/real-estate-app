06f2a0515ce8d4109d6dae56ad415cdb7a7c748b
Muhabat Khan Niazi <muhabatkhanx@gmail.com>
2022-09-22 09:04:48 -0700

    while navigate to another page scroll to top

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
+import ScrollTop from './components/scrollToTop/ScrollTop';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollTop/>
       <App />
     </BrowserRouter>
   </React.StrictMode>
